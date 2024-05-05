b306c9eb3b56224bcfb396a8f57c4062748a34fb
jett59 <jett.thompson.59@gmail.com>
2024-05-05 13:08:19 +1000

    add a simple egui test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use eframe::egui;
+use eframe::NativeOptions;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let native_options = NativeOptions::default();
+    eframe::run_native(
+        "Keyboard Layout Optimizer",
+        native_options,
+        Box::new(|_creation_context| Box::new(KeyboardLayoutOptimizerGui::default())),
+    )?;
+    Ok(())
+}
+
+#[derive(Default)]
+struct KeyboardLayoutOptimizerGui {}
+
+impl eframe::App for KeyboardLayoutOptimizerGui {
+    fn update(&mut self, context: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(context, |ui| {
+            let button1 = ui.button("Button 1");
+            if button1.clicked() {
+                println!("Button 1 clicked");
+            }
+        });
+    }
 }

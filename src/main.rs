d5dfeaaaafd90fbcd697f736b432d6b5ed581837
jett59 <jett.thompson.59@gmail.com>
2024-05-13 10:48:19 +1000

    add full keylogging capabilities for windows

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,17 @@ use std::error::Error;
 
 use eframe::egui;
 use eframe::NativeOptions;
-use windows::Win32::Foundation::HMODULE;
-use windows::Win32::Foundation::LPARAM;
-use windows::Win32::Foundation::LRESULT;
-use windows::Win32::Foundation::WPARAM;
-use windows::Win32::UI::WindowsAndMessaging::CallNextHookEx;
-use windows::Win32::UI::WindowsAndMessaging::SetWindowsHookExA;
-use windows::Win32::UI::WindowsAndMessaging::KBDLLHOOKSTRUCT;
-use windows::Win32::UI::WindowsAndMessaging::WH_KEYBOARD_LL;
-use windows::Win32::UI::WindowsAndMessaging::WM_KEYDOWN;
-use windows::Win32::UI::WindowsAndMessaging::WM_SYSKEYDOWN;
+use trace::Tracer;
 
-unsafe extern "system" fn hook_proc(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    if code >= 0 {
-        if wparam.0 as u32 == WM_KEYDOWN || wparam.0 as u32 == WM_SYSKEYDOWN {
-            println!("Key pressed");
-            let key_info = *(lparam.0 as *const KBDLLHOOKSTRUCT);
-            println!("Scan code: {}", key_info.scanCode);
-        }
-    }
-    CallNextHookEx(None, code, wparam, lparam)
-}
+mod keyboard;
+
+#[cfg_attr(windows, path = "windows/trace.rs")]
+mod trace;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    unsafe { SetWindowsHookExA(WH_KEYBOARD_LL, Some(hook_proc), HMODULE::default(), 0)? };
+    let tracer = Tracer::new(Box::new(|key_code| {
+        println!("{:?}", key_code);
+    }));
     let native_options = NativeOptions::default();
     eframe::run_native(
         "Keyboard Layout Optimizer",
---
+++ src/windows/trace.rs
@@ -0,0 +1,110 @@
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Mutex,
+};
+
+use windows::Win32::{
+    Foundation::{HINSTANCE, LPARAM, LRESULT, WPARAM},
+    UI::{
+        Input::KeyboardAndMouse::{
+            VIRTUAL_KEY, VK_A, VK_B, VK_C, VK_D, VK_E, VK_F, VK_G, VK_H, VK_I, VK_J, VK_K, VK_L,
+            VK_M, VK_N, VK_O, VK_OEM_1, VK_OEM_2, VK_OEM_COMMA, VK_OEM_PERIOD, VK_P, VK_Q, VK_R,
+            VK_S, VK_T, VK_U, VK_V, VK_W, VK_X, VK_Y, VK_Z,
+        },
+        WindowsAndMessaging::{
+            CallNextHookEx, SetWindowsHookExA, KBDLLHOOKSTRUCT, WH_KEYBOARD_LL, WM_KEYDOWN,
+            WM_SYSKEYDOWN,
+        },
+    },
+};
+
+use crate::keyboard::KeyCode;
+
+pub type CallbackFunction = Box<dyn FnMut(KeyCode) -> () + Send>;
+
+static CALLBACK_HANDLERS: Mutex<Option<Vec<CallbackFunction>>> = Mutex::new(None);
+
+unsafe extern "system" fn keyboard_hook_callback(
+    code: i32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
+    if code >= 0 {
+        let event_type = wparam.0 as u32;
+        if event_type == WM_KEYDOWN || event_type == WM_SYSKEYDOWN {
+            let event_info = *(lparam.0 as *const KBDLLHOOKSTRUCT);
+            if let Some(key_code) = match VIRTUAL_KEY(event_info.vkCode as u16) {
+                VK_Q => Some(KeyCode::Q),
+                VK_W => Some(KeyCode::W),
+                VK_E => Some(KeyCode::E),
+                VK_R => Some(KeyCode::R),
+                VK_T => Some(KeyCode::T),
+                VK_Y => Some(KeyCode::Y),
+                VK_U => Some(KeyCode::U),
+                VK_I => Some(KeyCode::I),
+                VK_O => Some(KeyCode::O),
+                VK_P => Some(KeyCode::P),
+                VK_A => Some(KeyCode::A),
+                VK_S => Some(KeyCode::S),
+                VK_D => Some(KeyCode::D),
+                VK_F => Some(KeyCode::F),
+                VK_G => Some(KeyCode::G),
+                VK_H => Some(KeyCode::H),
+                VK_J => Some(KeyCode::J),
+                VK_K => Some(KeyCode::K),
+                VK_L => Some(KeyCode::L),
+                // On standard US QWERTY, the ';' key is mapped to VK_OEM_1 for some reason.
+                VK_OEM_1 => Some(KeyCode::Semicolon),
+                VK_Z => Some(KeyCode::Z),
+                VK_X => Some(KeyCode::X),
+                VK_C => Some(KeyCode::C),
+                VK_V => Some(KeyCode::V),
+                VK_B => Some(KeyCode::B),
+                VK_N => Some(KeyCode::N),
+                VK_M => Some(KeyCode::M),
+                VK_OEM_COMMA => Some(KeyCode::Comma),
+                VK_OEM_PERIOD => Some(KeyCode::Dot),
+                // '/' is also OEM_2.
+                VK_OEM_2 => Some(KeyCode::Slash),
+                _ => None,
+            } {
+                let mut callbacks = CALLBACK_HANDLERS.lock().unwrap();
+                if let Some(callbacks) = &mut *callbacks {
+                    for callback in callbacks {
+                        callback(key_code);
+                    }
+                }
+            }
+        }
+    }
+    CallNextHookEx(None, code, wparam, lparam)
+}
+
+pub struct Tracer();
+
+impl Tracer {
+    pub fn new(callback: CallbackFunction) -> Self {
+        static ADDED_HOOK: AtomicBool = AtomicBool::new(false);
+        // If we haven't already registered the global key hook, do it here.
+        if ADDED_HOOK
+            .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
+            .is_ok()
+        {
+            unsafe {
+                SetWindowsHookExA(
+                    WH_KEYBOARD_LL,
+                    Some(keyboard_hook_callback),
+                    HINSTANCE::default(),
+                    0,
+                )
+                .unwrap()
+            };
+        }
+        let mut handlers = CALLBACK_HANDLERS.lock().unwrap();
+        if handlers.is_none() {
+            *handlers = Some(Vec::new());
+        }
+        handlers.as_mut().unwrap().push(callback);
+        Self()
+    }
+}
